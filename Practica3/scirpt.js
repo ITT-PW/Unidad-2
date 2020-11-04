c1fd1af5a6339c6221b7ccf43bb587f24b6888b5
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-11-03 18:47:55 -0800

    Update unidad  menu grid

---
+++ Practica3/scirpt.js
@@ -0,0 +1,13 @@
+// Typescript
+
+const usuario={
+    nombre:'julio',
+    apellido:'chavez'
+};
+
+console.log(usuario);
+console.log(usuario.apellido);
+
+const pelicula={"Title":"Avenger",
+"Year":"2006","Rated":"Not Rated","Released":"09 Apr 2006","Runtime":"92 min","Genre":"Thriller","Director":"Robert Markowitz","Writer":"Alan Sharp (teleplay), Frederick Forsyth (novel)","Actors":"Sam Elliott, Timothy Hutton, James Cromwell, William Hope","Plot":"A C.I.A. Agent steps in to stop a former Special Forces Operative on a for-hire mission that poses a global threat.","Language":"English","Country":"USA","Awards":"N/A","Poster":"https://m.media-amazon.com/images/M/MV5BMTMzMjMwMjcyNl5BMl5BanBnXkFtZTcwNTA1NDgzMQ@@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"5.6/10"},{"Source":"Rotten Tomatoes","Value":"27%"}],"Metascore":"N/A","imdbRating":"5.6","imdbVotes":"912","imdbID":"tt0473445","Type":"movie","DVD":"N/A","BoxOffice":"N/A","Production":"Radiant Productions","Website":"N/A","Response":"True"};
+console.log(pelicula);
